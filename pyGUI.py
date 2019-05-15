1ca6bde22ea86e7d7768b067c10c14d593b97fea
rsobrepe <raymon.sobrepena@ryerson.ca>
2019-05-15 16:58:31 -0400

    Update GUI file

--- pyGUI.py
+++ pyGUI.py
@@ -4,46 +4,23 @@ from tkinter import Tk
 from tkinter import ttk
 from tkinter import *
 
+def square (event):
+    a=int(num1.get())
+    b=a*a
+    result.delete(0,"end")
+    result.insert(0,b)
+
 root = Tk()
 
+Label(root, text="Find the square of a number").pack()
+num1=Entry(root)
+num1.pack(side=LEFT)
 
-'''
-root.title("First GUI using python")
-ttk.Button(root, text="Hello World").grid()
-root.mainloop()
+btn=Button(root, text="Squares to")
+btn.bind("<Button-1>", square)
+btn.pack(side=LEFT)
 
-
-frame = Frame(root)
-labelText = StringVar()
-
-label = Label(frame, textvariable= labelText)
-button = Button(frame, text="I am a button")
-
-labelText.set("Hey! Whatsup")
-
-label.pack()
-button.pack()
-frame.pack()
+result= Entry(root)
+result.pack(side=LEFT)
 
 root.mainloop()
-
-
-frame=Frame(root)
-Label(frame, text="hi").pack()
-Button(frame, text="b1").pack(side=LEFT, fill=Y)
-Button(frame, text="b2").pack(side=RIGHT, fill=X)
-Button(frame, text="b3").pack(side=TOP, fill=Y)
-Button(frame, text="b4").pack(side=LEFT, fill=X)
-Button(frame, text="b5").pack(side=RIGHT, fill=Y)
-
-frame.pack()
-root.mainloop()
-
-'''
-
-Label(root, text="Name").grid(row=0, column=0, sticky=N)
-Entry(root, width=50).grid(row=0, column=1)
-Button(root, text="submit").grid(row=0, column=5)
-
-root.mainloop()
-
